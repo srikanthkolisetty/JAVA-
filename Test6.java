c6fe5f734acf327151d2bc687856b3593d6b85b9
srikanthkolisetty <kolisettysrikanth2@gmail.com>
2022-10-25 19:18:26 +0530

    Java files

---
+++ Test6.java
@@ -0,0 +1,13 @@
+
+public class Test6 {
+	public static void main(String args[]) 
+	{	
+		int n=4;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+				System.out.print(i+1);
+			System.out.println();
+		}
+	}
+}
